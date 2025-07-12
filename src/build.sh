73ee6297439e5f3c4e9bce19c2dc01af7beb1319
Kishor Prins <me@68x.ca>
2025-07-12 07:55:55 -0700

    Fix install requirementx txt path in build.sh

--- src/build.sh
+++ src/build.sh
@@ -2,5 +2,5 @@
 
 conda init bash
 source activate py_3.10
-pip install -r /app/src/wyoming_piper/install_requirements.txt
+pip install -r /app/src/wyoming_whisper/install_requirements.txt
 pip install /app/ctranslate2-dist/*.whl
